ea072c7d74363cbc37f19ebc76aa471a02682f56
Travis CI <keith.miyake@gmail.com>
2018-03-06 02:16:54 +0000

    Generated Jekyll site by Travis CI - 48

--- README.md
+++
@@ -1,9 +0,0 @@
-[![Build Status](https://travis-ci.org/kaymmm/keithmiyake.info.svg?branch=master)](https://travis-ci.org/kaymmm/keithmiyake.info)
-
-personal website for keith miyake https://keithmiyake.info
-
-# LICENSE
-
-GNU GENERAL PUBLIC LICENSE GPLv3.0
-
-See [LICENSE.md](LICENSE.md).
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,56 @@
+var store = [{
+        "title": "3D Printing and Maker Culture",
+        "excerpt":"The GC Digital Fellows program recently acquired a 3D printer. For those unfamiliar with the technology, think of a typical ink jet printer, but instead of printing with ink, they spit out melted plastic. And instead of printing on a single sheet of paper, imagine printing hundreds of layers of...","categories": ["Digital Humanities"],
+        "tags": ["3D Printing","digital humanities","maker culture"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/3d-printing-and-maker-culture/",
+        "teaser":null},{
+        "title": "All That is Digital Melts into Code",
+        "excerpt":"One of the longstanding debates within the world of Digital Humanities is the question of “coding.” I’m going to take a different approach—rather than insert myself into that particular debate, I want to explore the much more basic question, “What does it mean to ‘code’?” I figure that since so...","categories": ["Digital Humanities"],
+        "tags": ["coding","digital humanities","modernity"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/all-that-is-digital-melts-into-code/",
+        "teaser":null},{
+        "title": "Learn Bootstrap Part 1: Getting Acquainted with Bootstrap",
+        "excerpt":"This is the first in a series of posts that will serve as a mini-course in using the Bootstrap web framework to eventually build a custom WordPress theme.What is Bootstrap?Bootstrap is an open-source project to create flexible design and layout components for the web. It is basically a bunch of...","categories": ["Digital Humanities"],
+        "tags": ["bootstrap","wordpress"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/getting-acquainted-with-bootstrap/",
+        "teaser":null},{
+        "title": "Learn Bootstrap Part 2: Adding Bootstrap to WordPress",
+        "excerpt":"This post is the second in a series of lessons on using the Bootstrap web framework to develop a WordPress theme.In the previous lesson we started to get a feel for what Bootstrap looks like in a web browser and began to think about using Bootstrap code snippets to lay...","categories": ["Digital Humanities"],
+        "tags": ["bootstrap","wordpress"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/learn-bootstrap-part-2-adding-bootstrap-to-wordpress/",
+        "teaser":null},{
+        "title": "Getting Started with Git and GitHub",
+        "excerpt":"One of my responsibilities as a CUNY Digital Fellow is to lead workshops for the GC community on various topics related to the Digital Humanities. Previously, I worked with Erin Glass to run a workshop on data management tools (check out the wiki we created for the workshop) and one...","categories": ["Digital Humanities"],
+        "tags": ["digital fellows","digital humanities","github"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/getting-started-with-git-and-github/",
+        "teaser":null},{
+        "title": "Learn Bootstrap Part 3: Customize Bootstrap and Add a Header",
+        "excerpt":"This is the third in a series of lessons on using the Bootstrap web framework to develop a WordPress theme.In the first lesson, we started to get a feel for what Bootstrap looks like in a web browser and began to think about using Bootstrap code snippets to lay out...","categories": ["Digital Humanities"],
+        "tags": ["bootstrap","digital humanities","LESS","wordpress"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/learn-bootstrap-part-3-customize-bootstrap-and-add-a-header/",
+        "teaser":null},{
+        "title": "PSA: Backup Your Data Now",
+        "excerpt":"Backup. It’s one of those anxiety provoking dirty words. We all want to feel happily ignorant that the possibility of losing our precious data could actually happen to us. And this is despite the fact that most everyone has lost some sort of irreplaceable data at some point in their...","categories": ["Digital Humanities"],
+        "tags": ["data"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/psa-backup-your-data-now/",
+        "teaser":null},{
+        "title": "Learning to Map with ArcGIS StoryMaps",
+        "excerpt":"IntroductionMaps are extremely versatile tools for communicating ideas and information. Creating maps can be challenging, but this tutorial will introduce one tool that simplifies the process of making several types of maps in order to tell your stories.The tool is called StoryMaps, and it was created by ESRI, the company...","categories": ["Digital Humanities","Geography"],
+        "tags": ["arcgis","mapping"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/geography/learning-to-map-with-arcgis-storymaps/",
+        "teaser":null},{
+        "title": "DH Strangelove or: How I Learned to Stop Worrying and Love the Digital Humanities",
+        "excerpt":"Disclaimer: I’m not trying to equate the Digital Humanities with Weapons of Mass Destruction. Just in case I needed to clarify my Kubrick reference…The start of a new academic (and fellowship) year prompts some introspection to establish goals and objectives for the year to come. Since I was new to...","categories": ["Digital Humanities"],
+        "tags": ["academia","digital humanities","maker culture"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/dh-strangelove-or-how-i-learned-to-stop-worrying-and-love-the-digital-humanities/",
+        "teaser":null},{
+        "title": "Command Line Workshop (tutorial)",
+        "excerpt":"IntroI recently gave a workshop on using the command line (a.k.a., terminal, command prompt, shell, etc.) at the Graduate Center as part of my Digital Fellow responsibilities. For the sake of sharing, I’m reposting the tutorial/walkthrough that guided the workshop. It’s really basic and pretty sparse, but the resources linked...","categories": ["Digital Humanities"],
+        "tags": ["command line","digital humanities","tutorial","workshop"],
+        "url": "https://keithmiyake.info/blog/digital%20humanities/command-line-workshop-tutorial/",
+        "teaser":null},{
+        "title": "Intro to Mapping with CartoDB",
+        "excerpt":"Why CartoDBAbout a year ago, I posted a brief tutorial on ESIR’s StoryMaps, a lightweight ArcGIS web mapping platform. This time around, I want to share a similar tutorial on the CartoDB platform. Whereas StoryMaps is a very minimal mapping platform designed to let users craft a narrative around a...","categories": [],
+        "tags": ["mapping","digital humanities"],
+        "url": "https://keithmiyake.info/blog/intro-to-mapping-with-cartodb/",
+        "teaser":null}]
